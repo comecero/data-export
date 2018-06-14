7b13eceb62cb0cc6047443a2990b356d4035d559
Chuck Larson <chuck-github@chlarson.com>
2018-06-14 17:44:12 -0500

    allow timezone to be configured in settings

--- app/pages/rootController.js
+++ app/pages/rootController.js
@@ -8,92 +8,95 @@ app.controller("TimezonesController", function($scope, TimezonesService) {
 });
 
 app.controller("RootController",
-  function ($scope, ApiService, $q, toCSV, saveFile, buildRootUrl, fetchData) {
-      // Set defaults
-      $scope.options = {
-        dataset: 'orders',
-        format: 'csv',
-        timezone: "UTC",
-        dates: "last_30",
-        unravelFields: [{id: 'items', name: 'Items'}],
-        unravelField: 'items'
-      };
-      $scope.datepicker = {
-        status: {
-          date_start: {opened: false},
-          date_end: {opened: false}
-        },
-        options: {
-          startingDay: 1,
-          showWeeks: false,
-          initDate: new Date(),
-          yearRange: 10
-        },
-        open: function ($event, which) {
-          $scope.datepicker.status[which].opened = true;
-        }
-      };
+  function ($scope, ApiService, $q, toCSV, saveFile, buildRootUrl, fetchData, SettingsService) {
+    var settings = SettingsService.get();
+    if (angular.isUndefined(settings.timezone)) settings.timezone = 'UTC';
 
-      var _cancel = function() {
-        if (angular.isFunction($scope.cancelFunc)) {
-          $scope.cancelFunc();
-          $scope.cancelFunc = undefined;
-          return true;
-        }
-        return false;
+    // Set defaults
+    $scope.options = {
+      dataset: 'orders',
+      format: 'csv',
+      timezone: settings.timezone,
+      dates: "last_30",
+      unravelFields: [{id: 'items', name: 'Items'}],
+      unravelField: 'items'
+    };
+    $scope.datepicker = {
+      status: {
+        date_start: {opened: false},
+        date_end: {opened: false}
+      },
+      options: {
+        startingDay: 1,
+        showWeeks: false,
+        initDate: new Date(),
+        yearRange: 10
+      },
+      open: function ($event, which) {
+        $scope.datepicker.status[which].opened = true;
       }
+    };
 
-      var _export = function(options, datepicker) {
-        fetchData($scope, options, datepicker).then(function(data) {
-          if (!angular.isArray(data) || data.length <= 0) {
-            $scope.error = {message: 'No results found'};
+    var _cancel = function() {
+      if (angular.isFunction($scope.cancelFunc)) {
+        $scope.cancelFunc();
+        $scope.cancelFunc = undefined;
+        return true;
+      }
+      return false;
+    }
+
+    var _export = function(options, datepicker) {
+      fetchData($scope, options, datepicker).then(function(data) {
+        if (!angular.isArray(data) || data.length <= 0) {
+          $scope.error = {message: 'No results found'};
+          return;
+        }
+
+        var formatted;
+        var type = 'text/' + options.format;
+        switch (options.format) {
+          case 'csv':
+            formatted = toCSV(data, options);
+            break;
+          case 'json':
+            formatted = JSON.stringify(data, function(key, value) {return value;}, new Number(2));
+            break;
+          default:
+            $scope.error = {message: 'Unknown export format'};
             return;
-          }
+        };
 
-          var formatted;
-          var type = 'text/' + options.format;
-          switch (options.format) {
-            case 'csv':
-              formatted = toCSV(data, options);
-              break;
-            case 'json':
-              formatted = JSON.stringify(data, function(key, value) {return value;}, new Number(2));
-              break;
-            default:
-              $scope.error = {message: 'Unknown export format'};
-              return;
-          };
-
-          var date_range = options.dates;
-          if (date_range == 'custom') {
-            date_range = datepicker.date_start.toISOString().substring(0, 10) + '_to_' +
-              datepicker.date_end.toISOString().substring(0, 10);
-          }
-
-          var fileName = options.dataset;
-          if (angular.isDefined(options.status) && options.status.length > 0) fileName += '_' + options.status;
-          fileName += '_' + date_range + '.' + options.format;
-          saveFile(fileName, type, formatted);
-          $scope.successMessage = 'Export successful';
-        }, function(error) {
-          $scope.error = error;
-          _cancel();
-        });
-      };
-
-      $scope.export = function() {
-        $scope.clearMessages();
-        _export(angular.copy($scope.options),angular.copy($scope.datepicker));
-      };
-
-      $scope.cancel = function() {
-        if (_cancel()) {
-          $scope.successMessage = 'Canceled export.';
+        var date_range = options.dates;
+        if (date_range == 'custom') {
+          date_range = datepicker.date_start.toISOString().substring(0, 10) + '_to_' +
+            datepicker.date_end.toISOString().substring(0, 10);
         }
-      }
 
-      $scope.clearMessages = function() {
-        $scope.successMessage = '';
-        $scope.error = {};
-      };
-  });
+        var fileName = options.dataset;
+        if (angular.isDefined(options.status) && options.status.length > 0) fileName += '_' + options.status;
+        fileName += '_' + date_range + '.' + options.format;
+        saveFile(fileName, type, formatted);
+        $scope.successMessage = 'Export successful';
+      }, function(error) {
+        $scope.error = error;
+        _cancel();
+      });
+    };
+
+    $scope.export = function() {
+      $scope.clearMessages();
+      _export(angular.copy($scope.options),angular.copy($scope.datepicker));
+    };
+
+    $scope.cancel = function() {
+      if (_cancel()) {
+        $scope.successMessage = 'Canceled export.';
+      }
+    }
+
+    $scope.clearMessages = function() {
+      $scope.successMessage = '';
+      $scope.error = {};
+    };
+});
